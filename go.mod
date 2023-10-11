c7b66c5efbadccb95581a3729b80583fa1dc295f
roman.fomindev <roman.fomindev@onlinesup.com>
2023-10-11 22:01:56 +0300

    week-2

--- go.mod
+++ go.mod
@@ -4,12 +4,22 @@ go 1.20
 
 require (
 	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	github.com/jackc/pgx/v4 v4.18.1
+	github.com/joho/godotenv v1.5.1
 	google.golang.org/grpc v1.58.2
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
--- internal/handlers/user_v1/user_v1.go
+++ internal/handlers/user_v1/user_v1.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"context"
 	"github.com/brianvoe/gofakeit"
+	"github.com/romanfomindev/microservices-auth/internal/managers"
+	"github.com/romanfomindev/microservices-auth/internal/models"
 	desc "github.com/romanfomindev/microservices-auth/pkg/user_v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -11,6 +13,13 @@ import (
 
 type UserV1Service struct {
 	desc.UnimplementedUserV1Server
+	manager *managers.UserManager
+}
+
+func NewUserService(manager *managers.UserManager) desc.UserV1Server {
+	return &UserV1Service{
+		manager: manager,
+	}
 }
 
 func (s *UserV1Service) Create(ctx context.Context, request *desc.CreateRequest) (*desc.CreateResponse, error) {
@@ -22,23 +31,32 @@ func (s *UserV1Service) Create(ctx context.Context, request *desc.CreateRequest)
 		request.GetInfo().GetRole(),
 	)
 
+	lastInsertId, err := s.manager.Create(
+		ctx,
+		request.GetInfo().GetName(),
+		request.GetInfo().GetEmail(),
+		request.GetInfo().GetPassword(),
+		models.ROLE_USER,
+	)
+
 	return &desc.CreateResponse{
-		Id: 123,
-	}, nil
+		Id: lastInsertId,
+	}, err
 }
 
 func (s *UserV1Service) Get(ctx context.Context, request *desc.GetRequest) (*desc.GetResponse, error) {
 	log.Printf("ID: %d\n", request.GetId())
 
+	user := s.manager.GetById(ctx, request.GetId())
+
+	/** TODO трансформатор с grpc to json */
 	return &desc.GetResponse{
 		User: &desc.User{
 			Id: request.GetId(),
 			Info: &desc.UserInfo{
-				Name:            gofakeit.Name(),
-				Email:           gofakeit.Email(),
-				Password:        "password",
-				PasswordConfirm: "password",
-				Role:            desc.Roles_USER,
+				Name:  user.Name,
+				Email: user.Email,
+				Role:  desc.Roles_USER,
 			},
 			CreatedAt: timestamppb.New(gofakeit.Date()),
 			UpdatedAt: timestamppb.New(gofakeit.Date()),
@@ -54,11 +72,23 @@ func (s *UserV1Service) Update(ctx context.Context, request *desc.UpdateRequest)
 		request.Role,
 	)
 
+	err := s.manager.Update(ctx, request.GetId(), request.GetName().GetValue(), request.GetEmail().GetValue(), models.ROLE_USER)
+
+	if err != nil {
+		return &emptypb.Empty{}, err
+	}
+
 	return &emptypb.Empty{}, nil
 }
 
 func (s *UserV1Service) Delete(ctx context.Context, request *desc.DeleteRequest) (*emptypb.Empty, error) {
 	log.Printf("id: %d", request.GetId())
+
+	err := s.manager.Delete(ctx, request.GetId())
+
+	if err != nil {
+		return &emptypb.Empty{}, err
+	}
 
 	return &emptypb.Empty{}, nil
 }
