6714d5e461b209db0b000182dfe9b078e72ae856
romanfomindev <roman.fomindev@onlinesup.com>
2023-11-15 18:23:03 +0300

    fix

--- internal/services/auth/get_access_token.go
+++ internal/services/auth/get_access_token.go
@@ -2,16 +2,15 @@ package auth
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/romanfomindev/microservices-auth/internal/utils"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (s *authService) GetAccessToken(ctx context.Context, refreshToken string) (string, error) {
 	claims, err := utils.VerifyToken(refreshToken, []byte(s.refreshTokenSecretKey))
 	if err != nil {
-		return "", status.Errorf(codes.Unauthenticated, "invalid token: %s", err.Error())
+		return "", fmt.Errorf("invalid refresh token: %w", err)
 	}
 
 	user, err := s.repo.GetByEmail(ctx, claims.Email)
